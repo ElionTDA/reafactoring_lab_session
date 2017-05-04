f16647ca083fdbeeb680431f5d84800176a6365c
DanielSantidrian <dsa0016@alu.ubu.es>
2017-05-04 19:05:46 +0200

    Codigo refactorizado moviendo el método printOn de la clase Network a la clase Node.

--- src/lanSimulation/tests/LANTests.java
+++ src/lanSimulation/tests/LANTests.java
@@ -213,7 +213,7 @@ the tests should work as expected.
 
 		try {
 			buf.append("---------------------------------ASCII------------------------------------------\n");
-			network.printOn(buf);
+			network.firstNode_.printOn(network, buf);
 			buf.append("\n\n---------------------------------HTML------------------------------------------\n");
 			network.printHTMLOn(buf);
 			buf.append("\n\n---------------------------------XML------------------------------------------\n");
